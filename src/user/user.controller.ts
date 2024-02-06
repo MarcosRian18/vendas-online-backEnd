c14ae3f414b690681bb97ac8e7f75c697f3903ad
MarcosRian18 <marcosrian.rian@gmail.com>
2024-02-06 02:38:43 -0300

    feat: Adiciona metodo get e services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
+
 @Controller('user')
 export class UserController {
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({ test: 'abc' })
-    }
+    constructor(private readonly userService: UserService) {}
 
+    @Get()
+    async getAllUsers(): Promise<User[]> {
+        return this.userService.getAlUser()
+    }
     @Post()
     async createUser(@Body() createUser: CreateUserDto) {
-        return{ ...createUser,
-        password: undefined}
+        return this.userService.createUser(createUser)
     }
-
 }
